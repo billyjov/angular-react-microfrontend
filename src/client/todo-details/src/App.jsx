c3662b8cb2bd7d2143be310edbcb274a526b18e8
Billy Lando <billy_lando@yahoo.com>
2019-01-21 00:43:16 +0100

    feature(rx:counter): display the first version of counter into react app

--- src/client/todo-details/src/App.jsx
+++ src/client/todo-details/src/App.jsx
@@ -2,15 +2,32 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+    constructor(props) {
+        super(props)
+        this.state = { totalDoneTasks: 0 };
+    }
+
+    componentDidMount() {
+        this.nv.addEventListener('allDoneTasksEmitter', this.handleNvEnter);
+    }
+
+    componentWillUnmount() {
+        this.nv.removeEventListener('allDoneTasksEmitter', this.handleNvEnter);
+    }
+
+    handleNvEnter = (event) => {
+        this.setState({ totalDoneTasks: event.detail.length });
+    }
+
     render() {
         return (
             <div className="App">
                 <header className="App-header">
-                    <p>
-                        React app sucessfully loaded
-                    </p>
+                    <h1>
+                        React app sucessfully loaded: {this.state.totalDoneTasks} done tasks
+                    </h1>
                 </header>
-                <app-ng-todolist></app-ng-todolist>
+                <app-ng-todolist ref={elem => this.nv = elem}></app-ng-todolist>
             </div>
         );
     }
