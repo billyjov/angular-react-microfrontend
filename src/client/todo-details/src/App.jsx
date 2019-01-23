2a932dda22bb8aaf5a526e2e4e772f72bdf94bb4
Billy Lando <billy_lando@yahoo.com>
2019-01-23 08:56:11 +0100

    feature(rx): improve react app template

--- src/client/todo-details/src/App.jsx
+++ src/client/todo-details/src/App.jsx
@@ -1,36 +1,106 @@
 import React, { Component } from 'react';
 import './App.css';
+import logo from './logo.svg';
 
-class App extends Component {
+export default class App extends Component {
     constructor(props) {
         super(props)
-        this.state = { totalDoneTasks: 0 };
+        this.state = {
+            allTasks: [],
+            totalDoneTasks: [],
+            taskForToday: []
+        };
     }
 
     componentDidMount() {
-        this.nv.addEventListener('allDoneTasksEmitter', this.handleNvEnter);
+        this.nv.addEventListener('ngAllTasksEmitter', this.handleTasksEvent);
     }
 
     componentWillUnmount() {
-        this.nv.removeEventListener('allDoneTasksEmitter', this.handleNvEnter);
+        this.nv.removeEventListener('ngAllTasksEmitter', this.handleTasksEvent);
     }
 
-    handleNvEnter = (event) => {
-        this.setState({ totalDoneTasks: event.detail.length });
+    handleTasksEvent = (tasksEvent) => {
+        console.log('event got: ', tasksEvent);
+        const allTasks = tasksEvent.detail;
+        const allDoneTasks = this.getAllDoneTasks(allTasks);
+        const taskForToday = this.getTodayDueDateTasks(allTasks)
+        this.setState({
+            allTasks: allTasks,
+            totalDoneTasks: allDoneTasks,
+            taskForToday: taskForToday
+        });
+    }
+
+    getAllDoneTasks = (allTasks) => {
+        const allDoneTasks = allTasks.filter(tasks => tasks.done);
+        return allDoneTasks;
+    }
+
+    getTodayDueDateTasks = (allTasks) => {
+        const today = new Date().toDateString();
+        console.log('all task for filter today', allTasks);
+        console.log('today is', today);
+        const taskForToday = allTasks.filter(tasks => (tasks.dueDate) === today);
+        console.log('task for today ', taskForToday);
+        return taskForToday;
     }
 
     render() {
         return (
-            <div className="App">
-                <header className="App-header">
-                    <h1>
-                        React app sucessfully loaded: {this.state.totalDoneTasks} done tasks
-                    </h1>
+            <div className="App mb-5">
+                <header className="App-header mb-5">
+                    <nav className="navbar navbar-light bg-info">
+                        <a className="navbar-brand" href=""><img src={logo} className="App-logo" alt="logo" /></a>
+                        <div className="text-right">
+                            <span className="navbar-icon--container">
+                                <i className="material-icons navbar-icon">
+                                    assignment
+                                </i>
+                                <span className="navbar-icon--badge">{this.state.allTasks.length}</span>
+                            </span>
+                            <span className="navbar-icon--container">
+                                <i className="material-icons navbar-icon">
+                                    assignment_turned_in
+                                </i>
+                                <span className="navbar-icon--badge">{this.state.totalDoneTasks.length}</span>
+                            </span>
+
+                        </div>
+                    </nav>
                 </header>
-                <app-ng-todolist ref={elem => this.nv = elem}></app-ng-todolist>
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-md-8">
+                            <app-ng-todolist ref={elem => this.nv = elem} reactEvent={this.state.totalDoneTasks}></app-ng-todolist>
+                        </div>
+                        <div className="col-md-4">
+                            <div className="card mb-5">
+                                <div className="card-header bg-info text-white">
+                                    <h4 className="text-uppercase">To do today</h4>
+                                </div>
+                                <div className="card-body">
+                                    <p className="alert alert-primary d-flex">
+                                        <i className="material-icons mr-2">visibility_off</i>
+                                        <span>Nothing to see yet</span>
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="card">
+                                <div className="card-header bg-info text-white">
+                                    <h4 className="text-uppercase">To do this week</h4>
+                                </div>
+                                <div className="card-body">
+                                    <p className="alert alert-primary d-flex">
+                                        <i className="material-icons mr-2">visibility_off</i>
+                                        <span>Nothing to see yet</span>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
 }
-
-export default App;
--- src/client/todo-details/src/index.js
+++ src/client/todo-details/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'material-icons/iconfont/material-icons.css';
 import './index.css';
 import App from './App.jsx';
 import * as serviceWorker from './serviceWorker';
