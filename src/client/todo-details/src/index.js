d4ef298e87eced1ba1ae75cf681809e89ee141d0
Billy Lando <billy_lando@yahoo.com>
2019-02-06 15:24:23 +0100

    feat(test):  set up enzyme adapter for react testing

--- src/client/todo-details/src/index.js
+++ src/client/todo-details/src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'material-icons/iconfont/material-icons.css';
 import './index.css';
+
 import App from './App.jsx';
+
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
